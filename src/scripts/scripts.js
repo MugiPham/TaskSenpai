e5bd40428e08170e7c328091f336b68c485b8ab4
Armagregon15 <70592112+Armagregon15@users.noreply.github.com>
2021-05-10 22:39:53 -0500

    fixed projects on home

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,18 +1,18 @@
 
-var tasks;
-const section = document.querySelector('section');
-document.getElementById("mySection").onload = function () { populateProjectList() };
-let requestURL = 'https://raw.githubusercontent.com/Armagregon15/Armagregon.github.io/master/project.json';
-let request = new XMLHttpRequest();
-request.open('GET', requestURL, true);
-request.responseType = 'json';
-request.send();
+// var tasks;
+// const section = document.querySelector('section');
+// document.getElementById("mySection").onload = function () { populateProjectList() };
+// let requestURL = 'https://raw.githubusercontent.com/Armagregon15/Armagregon.github.io/master/project.json';
+// let request = new XMLHttpRequest();
+// request.open('GET', requestURL, true);
+// request.responseType = 'json';
+// request.send();
 
-request.onload = function () {
-  const response = request.response;
-  console.log(response['projects'][0]['PName']);
-  populateProjectList(response);
+// request.onload = function () {
+//   const response = request.response;
+//   console.log(response['projects'][0]['PName']);
+//   populateProjectList(response);
 
-}
+// }
 
 
